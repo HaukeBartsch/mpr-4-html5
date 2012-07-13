35f2d52243c1a8f70cec68f52046cb11e5476eac
Hauke Bartsch <haukebartsch@gmail.com>
2012-07-13 08:35:36 -0700

    mpr.js: fix translation issue after pan

--- source/mpr.js
+++ source/mpr.js
@@ -249,7 +249,8 @@ if (!document.createElement('mpr').getContext) {
     jQuery(id).mousedown( (function(context) { return function(event) { 
 	context.isMouseDown = true;
         canvasLocation = findPos(this);
-	context.mousePos1 = [event.pageX - canvasLocation[0] /*this.offsetLeft*/, event.pageY - canvasLocation[1] /*this.offsetTop*/];
+	context.mousePos1 = [event.pageX - canvasLocation[0], 
+			     event.pageY - canvasLocation[1]];
 	context.startBrightnessContrast = [ context.getBrightness(), context.getContrast() ];
 	for (var i = 0; i < 6; i++) {
 	  context.translate[i] = 0;
@@ -417,35 +418,37 @@ if (!document.createElement('mpr').getContext) {
            new importImages( context.overlayDataPath, context.updateDataStore, context, context.position, false );
       }
    })(this));
-   jQuery(id).mousewheel((function(context) {
-      return function(event, delta) {
-               event.preventDefault();
-               var orientation = jQuery(this).attr('orientation');
-               var amount = Math.round(delta/0.3333);
-               if ( orientation == "0,0,1" ) {
-                  if (!context.lockPosition[0])
-                    context.position[0]+=amount;
-               }
-               if ( orientation == "0,1,0" ) {
-          if (!context.lockPosition[1])
-                     context.position[1]+=amount;
-               }
-               if ( orientation == "1,0,0" ) {
-                  if (!context.lockPosition[2])
-                    context.position[2]+=amount;
-               }
-               // normalize the position
-               for (var i = 0; i < 3; i++) {
-                  if (context.position[i] < 0)
-                     context.position[i] = 0;
-                  if (context.position[i] > context.getDims()[i]-1)
-                     context.position[i] = context.getDims()[i]-1;
-               }
-               new importImages( context.dataPath, context.updateDataStore, context, context.position, true );
-               if (context.overlayDataIsSet)
-                 new importImages( context.overlayDataPath, context.updateDataStore, context, context.position, false );
-      }
-   })(this));
+if (typeof jQuery(id).mousewheel == 'function') {  
+     jQuery(id).mousewheel((function(context) {
+	 return function(event, delta) {
+           event.preventDefault();
+         var orientation = jQuery(this).attr('orientation');
+	 var amount = Math.round(delta/0.3333);
+         if ( orientation == "0,0,1" ) {
+           if (!context.lockPosition[0])
+             context.position[0]+=amount;
+           }
+         if ( orientation == "0,1,0" ) {
+         if (!context.lockPosition[1])
+           context.position[1]+=amount;
+         }
+         if ( orientation == "1,0,0" ) {
+           if (!context.lockPosition[2])
+             context.position[2]+=amount;
+           }
+           // normalize the position
+           for (var i = 0; i < 3; i++) {
+	     if (context.position[i] < 0)
+               context.position[i] = 0;
+             if (context.position[i] > context.getDims()[i]-1)
+               context.position[i] = context.getDims()[i]-1;
+             }
+             new importImages( context.dataPath, context.updateDataStore, context, context.position, true );
+             if (context.overlayDataIsSet)
+               new importImages( context.overlayDataPath, context.updateDataStore, context, context.position, false );
+	 }
+     })(this));
+   }
    jQuery(id).keydown((function(context) {
       return function(event) {
          if (event.keyCode == 49) {
@@ -480,7 +483,9 @@ if (!document.createElement('mpr').getContext) {
         if (event.charCode == 48) { // reset zoom
           //context.setBrightness(0);
           //context.setContrast(0);
-            context.zoomFactor = 1.0;
+	  context.startTranslate = [0,0,0,0,0,0];
+	  context.translate      = [0,0,0,0,0,0];
+          context.zoomFactor     = 1.0;
         }
         if (event.charCode == 43 && !event.metaKey ) { // increase the zoom factor
           context.zoomFactor = context.zoomFactor * 1.1;
